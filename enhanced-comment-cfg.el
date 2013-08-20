7205fd6a6f0850621fff6f979e0a111421e2a4c1
jsuper <ling.java@gmail.com>
2013-08-20 17:39:27 +0800

    rename from *annotation.el to *comment*.el

---
+++ enhanced-comment-cfg.el
@@ -0,0 +1,42 @@
+;; Enhanced Annotation Configuration Data
+
+(setq mode-annotation-cfg
+      '(
+	(:name "emacs-lisp-mode"
+	       :support-block-annotation nil
+	       :annotation-symbol ";")
+	(:name "java-mode"
+	       :support-block-annotation t
+	       :annotation-symbol "//"
+	       :begin-block-symbol "/*"
+	       :end-block-symbol "*/")
+	(:name "nxml-mode"
+	       :support-block-annotation t
+	       :begin-block-symbol "<!--"
+	       :end-block-symbol "-->")
+	))
+
+(defun --mode-name-selector (mode-name)
+  #'(lambda (mode-cfg)
+      (equal (getf mode-cfg :name) mode-name)))
+
+(defun --query-mode-cfg (selector-fn)
+  (remove-if-not selector-fn mode-annotation-cfg))
+
+(defun query-cfg-by-mode-name (mode-name)
+  (first (--query-mode-cfg (--mode-name-selector mode-name))))
+
+(defun get-annotation-symbol (mode-cfg)
+  (getf mode-cfg :annotation-symbol))
+
+(defun get-suport-block-annotation (mode-cfg)
+  (getf mode-cfg :support-block-annotation))
+
+(defun get-begin-block-symbol (mode-cfg)
+  (getf mode-cfg :begin-block-symbol))
+
+(defun get-end-block-symbol (mode-cfg)
+  (getf mode-cfg :end-block-symbol))
+
+(provide 'mode-annotation-cfg)
+
---
+++ enhanced-comment.el
@@ -0,0 +1,56 @@
+;;Enhanced Annotation for Emacs User
+
+(require 'mode-annotation-cfg)
+
+(defun annotate-current-line (mode-cfg)
+  (let ((annotation-symbol (get-annotation-symbol mode-cfg))
+	(support-block-annotation (get-suport-block-annotation mode-cfg)))
+    (if annotation-symbol
+	(progn
+	  (goto-char (line-beginning-position))
+	  (insert-string annotation-symbol))
+      (progn
+	(if support-block-annotation
+	    (progn
+	      (goto-char (line-beginning-position))
+	      (insert-string (get-begin-block-symbol mode-cfg))
+	      (goto-char (line-end-position))
+	      (insert-string (get-end-block-symbol mode-cfg))))
+	(message "Cannot find avaiable annotation configuration")))))
+
+
+(defun annotate-region (start-pos end-pos line-count)
+  (let ((mode-cfg (query-cfg-by-mode-name (format "%s" major-mode))))
+    (message "Will annotate %d lines" line-count)
+    (if (equal 1 line-count)
+	(annotate-current-line mode-cfg)
+      (progn
+	(if (get-suport-block-annotation mode-cfg)
+	    (progn
+	      (goto-char start-pos)
+	      (goto-char (line-beginning-position))
+	      (insert-string (get-begin-block-symbol mode-cfg))
+	      (goto-char end-pos)
+	      (goto-char (line-end-position))
+	      (insert-string (get-end-block-symbol mode-cfg)))
+	  (progn
+	    (setq current-line-pos start-pos)
+	    (dotimes (i line-count)
+	      (goto-char current-line-pos)
+	      (goto-char (line-beginning-position))
+	      (annotate-current-line mode-cfg)
+	      (setq current-line-pos (+ 1 (line-end-position))))))
+	))
+    ))
+
+(defun annotate-selection ()
+  (interactive)
+  (let ((line-count (count-lines (region-beginning) (region-end))))
+    (if (<= line-count 1)
+	(progn
+	  (goto-char (region-beginning))
+	  (goto-char (line-beginning-position))
+	  (annotate-region (line-beginning-position) (line-end-position) 1))
+      (annotate-region (region-beginning) (region-end) line-count))))
+
+(global-set-key (kbd "M-p") 'annotate-selection)
