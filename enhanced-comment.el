b59f1cf9d7a1fd5a26ee77735620b267a8a0f710
Tony <ling.java@gmail.com>
2013-09-01 17:58:15 +0800

    implemented all function for comment and uncomment

--- enhanced-comment.el
+++ enhanced-comment.el
@@ -1,8 +1,10 @@
 ;;Enhanced Annotation for Emacs User
 
 (require 'mode-comment-cfg)
+(require 'enhanced-common-function)
 
 (defun comment-current-line (mode-cfg)
+  "comment current line"
   (let ((comment-symbol (get-comment-symbol mode-cfg))
 	(support-block-comment (get-support-block-comment mode-cfg)))
     (if comment-symbol
@@ -20,6 +22,7 @@
 
 
 (defun comment-region (start-pos end-pos line-count)
+  "comment selection region, this will include the line which current cursor in"
   (let ((mode-cfg (query-cfg-by-mode-name (format "%s" major-mode))))
     (message "Will comment %d lines" line-count)
     (if (equal 1 line-count)
@@ -45,6 +48,7 @@
     ))
 
 (defun comment-selection ()
+  "interactive method for key-binding or interactive call"
   (interactive)
   (if (region-active-p)
       (let ((line-count (count-lines (region-beginning) (region-end))))
@@ -57,31 +61,30 @@
     (comment-region (line-beginning-position) (line-end-position) 1)))
 
 (defun uncomment-current-line (comment-cfg)
+  "Uncomment current line in buffer"
   (let ((comment-symbol (get-comment-symbol comment-cfg))
 	(support-block-comment (get-support-block-comment comment-cfg)))
     (if comment-symbol
-	(replace-string comment-symbol ""
-			nil
-			(line-beginning-position) 
-			(+ (line-beginning-position) (length comment-symbol)))
+	(progn 
+	  (goto-char (line-beginning-position))
+	  (delete-string-forward comment-symbol (line-end-position) 1))
       (if support-block-comment
 	  (let ((begin-symbol (get-begin-block-symbol comment-cfg))
 		(end-symbol (get-end-block-symbol comment-cfg)))
-	    (replace-string begin-symbol "" 
-			    nil
-			    (line-beginning-position)
-			    (+ (line-beginning-position) (length begin-symbol)))
-	    (replace-string end-symbol ""
-			    nil
-			    (- (line-end-position) (length end-symbol))
-			    (line-end-position)))
+	    (goto-char (line-beginning-position))
+	    (delete-string-forward begin-symbol (line-end-position) 1)
+	    (goto-char (line-end-position))
+	    (delete-string-backward end-symbol (line-beginning-position) 1)
+	    )
 	(message "not found avaiable commentary configuration for current major mode"))
 	))
   )
 
 (defun uncomment-region (start-pos end-pos line-count)
+
+  "Uncomment selection area in current buffer"
+
   (let ((mode-cfg (query-cfg-by-mode-name (format "%s" major-mode))))
-    (message "will uncomment %d lines" line-count)
     (if (equal 1 line-count)
 	(uncomment-current-line mode-cfg)
       (let ((comment-symbol (get-comment-symbol mode-cfg))
@@ -90,40 +93,34 @@
 	    (let ((begin-symbol (get-begin-block-symbol mode-cfg))
 		  (end-symbol (get-end-block-symbol mode-cfg)))
 	      (goto-char start-pos) ;;goto the region start position
-	      (goto-char (line-beginning-position))
-	      (replace-string begin-symbol "" 
-			      nil
-			      (line-beginning-position)
-			      (+ (line-beginning-position) (length begin-symbol)))
+	      (delete-string-forward begin-symbol (line-end-position) 1)
 	      (goto-char end-pos)
-	      (goto-char (line-end-position))
-	      (replace-string end-symbol ""
-			      nil
-			      (- (line-end-position) (length (end-symbol)))
-			      (line-end-position))
-	      )
+	      (delete-string-backward end-symbol start-pos 1))
 	  (progn
 	    (goto-char start-pos)
 	    (setq symbol-len (length comment-symbol))
 	    (dotimes (i line-count)
 	      (goto-char (line-beginning-position))
-	      (replace-string comment-symbol ""
-			      nil
-			      (line-beginning-position)
-			      (+ (line-beginning-position) symbol-len))
+	      (delete-string-forward comment-symbol (line-end-position) 1)
 	      (goto-char (+ 1 (line-end-position)))))
 	  )))))
 
 (defun uncomment-selection ()
+  "interactive uncomment method for interactive call or key-binding"
   (interactive)
   (if (region-active-p)
-      (let ((line-count (count-lines (region-beginning) (region-end))))
+      (let ((line-count (count-lines (region-beginning) (region-end)))
+	    (start-pos (region-beginning))
+	    (end-pos (region-end)))
 	(goto-char (region-beginning))
+	(setq start-pos (line-beginning-position))
+	(goto-char end-pos)
+	(setq end-pos (line-end-position))
 	(if (<= line-count 1)
 	    (progn
 	      (goto-char (region-beginning))
-	      (uncomment-region (line-beginning-position) (region-end) 1))
-	  (uncomment-region (line-beginning-position) (region-end) line-count)))
+	      (uncomment-region start-pos end-pos 1))
+	  (uncomment-region start-pos end-pos line-count)))
     (uncomment-region (line-beginning-position) (line-end-position) 1)
     ))
 
