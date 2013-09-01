a6a5210194ac40d3acd9bc4787134a3e47d17833
Tony <ling.java@gmail.com>
2013-09-01 21:02:02 +0800

    rename to enhanced-editor-setup.el

---
+++ enhanced-editor-setup.el
@@ -0,0 +1,14 @@
+
+;; key-binding for enhanced editor
+
+(require 'enhanced-comment)
+
+(global-unset-key (kbd "C-/"))
+(global-unset-key (kbd "C-\\"))
+
+(global-set-key (kbd "C-/") 'comment-selection)
+(global-set-key (kbd "C-\\") 'uncomment-selection)
+
+
+
+(provide 'enhanced-editor-setup)
